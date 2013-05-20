3b67e79f1be911e5889cab20e0c08d23ddc85cad
mkv25.user@gmail.com <mkv25.user@gmail.com@d893e8b9-1e8a-bbb3-b425-bbff04538be4>
2013-05-20 11:26:47 +0000

    R6 - Added Double Buffer property to main DataGridView to improve redraw time

---
+++ trunk/C#/Data Fairy/Data Fairy Editor/DoubleBufferedDataGrid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Data_Fairy_Editor
+{
+    public class DoubleBufferedDataGrid : DataGridView
+    {
+        public DoubleBufferedDataGrid()
+        {
+            DoubleBuffered = true;
+        }   
+    }
+}
--- trunk/C#/Data Fairy/Data Fairy Editor/SplashForm.cs
+++ trunk/C#/Data Fairy/Data Fairy Editor/SplashForm.cs
@@ -19,9 +19,10 @@ namespace Data_Fairy_Editor
 
             this.Icon = Icons.data_fairy;
 
-            versionLabel.Text = String.Format("Version {0}", "1.0.5");
+            versionLabel.Text = String.Format("Version {0}", "1.0.6");
 
             var releaseChanges = new List<string>();
+            releaseChanges.Add("R6 - Added Double Buffer property to main DataGridView to improve redraw time");
             releaseChanges.Add("R5 - Tweak to code generation");
             releaseChanges.Add("R4 - PHP Code Templates");
             releaseChanges.Add("R3 - Haxe Code Templates, ClickOnce deployment");
